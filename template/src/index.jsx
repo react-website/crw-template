ca95f957fda753f2ef14ca607c1d86fe505574e8
qq2575896094 <2575896094@qq.com>
2024-01-09 12:52:57 +0800

    feat(project): add template configuration

--- template/src/index.jsx
+++ template/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-// import { Provider } from 'react-redux'
-// import AppRoot from '@framework/layout/app-root'
+import { Provider } from 'react-redux'
+import AppRoot from '@framework/layout/app-root'
 
-// import store from '@framework/store'
+import store from '@framework/store'
 
 import './css/index.scss'
 
@@ -11,8 +11,8 @@ createRoot(document.getElementById('root'))
     .render(
         <React.StrictMode>
             hello world!
-            {/*<Provider store={store}>*/}
-            {/*    <AppRoot />*/}
-            {/*</Provider>*/}
+            <Provider store={store}>
+                <AppRoot />
+            </Provider>
         </React.StrictMode>,
     )
