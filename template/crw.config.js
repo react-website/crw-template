45390aba3121aa6086f7be484b5a4560cb2beccd
qq2575896094 <2575896094@qq.com>
2024-01-11 11:28:26 +0800

    add(project): add crw.config configuration

---
+++ template/crw.config.js
@@ -0,0 +1,23 @@
+/**
+ * @name: crw.config
+ * @description: 工程配置文件
+ * @author: qq2575896094
+ * @time: 2024/1/8
+ */
+
+const { resolve } = require('path')
+
+const resolveApp = (relativePath) => resolveApp(__dirname, 'src', relativePath)
+
+module.exports = () => ({
+    alias: {
+        '@': resolveApp('.'),
+        '@pages': resolveApp('pages'),
+        '@components': resolveApp('components'),
+        '@fonts': resolveApp('css/fonts'),
+        '@framework': resolveApp('framework'),
+    },
+    proxy: {
+        '/user': 'http://123.56.143.21:8090/user'
+    },
+})
